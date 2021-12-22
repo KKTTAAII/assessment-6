33dfcaadb2d21b7347c8a018d2754ec16b2996b6
KKTTAAII <krataias65@gmail.com>
2021-12-22 14:08:54 -0700

    part 3

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,39 @@
-const express = require('express');
-let axios = require('axios');
-var app = express();
+const express = require("express");
+const axios = require("axios");
+const middleware = require("./middleware");
+const app = express();
 
-app.post('/', function(req, res, next) {
+app.use(express.json());
+
+app.post("/", middleware.checkBodyReq, async (req, res, next) => {
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
 
-    return res.send(JSON.stringify(out));
-  } catch {
+    const promises = req.body.developers.map((developer) => {
+      return axios.get(`https://api.github.com/users/${developer}`);
+    });
+
+    const responses = await Promise.all(promises);
+
+    const devsData = responses.map((response) => ({
+      name: response.data.name,
+      bio: response.data.bio,
+    }));
+
+    return res.json(devsData);
+  } catch (err) {
     next(err);
   }
 });
 
-app.listen(3000);
+app.use((req, res, next) => {
+  const err = new Error("Page not found", 404);
+  next(err);
+});
+
+app.use((err, rep, res, next) => {
+  next(err);
+});
+
+app.listen(3000, () => {
+  console.log("Starting server 3000");
+});
